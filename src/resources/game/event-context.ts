50a175d293d68daae03bbe7180e74766ddb6f331
lucascyrne <cyrne24@gmail.com>
2025-06-20 00:53:14 -0300

    chore: deleted files

--- src/resources/game/event-context.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-
-export interface EventContextType {
-  interactWithEvent: () => Promise<void>;
-}
-
-export const EventContext = createContext<EventContextType | null>(null);
